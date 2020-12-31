eff456eec123566dd01a087c4c12006952f8903d
Shubham Kumar Burman <shubhamburman09@gmail.com>
2020-12-31 10:24:52 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class App extends Component {
-render() {
-  return (
-    <div>
-      <h1>Phone Directory</h1>
-    </div>
-  );
-}
+  render() {
+    return (
+      <div>
+        {/* <div classname="header">
+          <h1>Phone Directory</h1>
+        </div>
+        <button> Add </button>
+        <div>
+          <span> Name </span> <br />
+          <span> Phone </span>
+        </div>
+      </div> */}
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          type="text"
+          placeholder="Type Here"
+          defaultValue="Shubh"
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
