029cf285b28c6e14aa7bf3a9a062f826275fe340
Shubham Kumar Burman <shubhamburman09@gmail.com>
2021-01-09 12:18:29 +0530

    Styled ‘Header’ (Inline Styling)

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,14 @@
 import { render } from "@testing-library/react";
 import React, { Component } from "react";
-
+const headerStyle = {
+  textAlign: "center",
+  padding: 20,
+  background: "#000",
+  color: "#fff",
+  textTransform: "uppercase",
+};
 const Header = function () {
-  return <div className="header">Phone Directory</div>;
+  return <div style={headerStyle}>Phone Directory</div>;
 };
 
 export default Header;
