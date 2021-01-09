949532d4a6dd12d5eb413f8c3e2972c41ce7a911
Shubham Kumar Burman <shubhamburman09@gmail.com>
2021-01-09 12:57:12 +0530

    Styled 'App' (Home Page)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
 import Header from "./Header";
+import "./App.css";
 class App extends Component {
   render() {
     return (
       <div>
         <Header />
-        <button> Add </button>
-        <div>
-          <span> Name </span> <br />
-          <span> Phone </span>
+        <div className="component-body-container">
+          <button className="custom-btn add-btn"> Add </button>
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading"> Name </span>
+            <span className="grid-item phone-heading"> Phone </span>
+          </div>
         </div>
       </div>
     );
